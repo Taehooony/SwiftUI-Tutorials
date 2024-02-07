180527802d7f0a66a92a3d0804ed470aa5e6d3c6
Taehoon Kang <kywlk0709@naver.com>
2024-02-08 01:36:30 +0900

    first commit
    섹션3 9단계 까지 완료

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 강 태훈 on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
